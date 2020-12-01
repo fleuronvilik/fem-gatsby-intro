00b68117f310d901f5be6765e80471026452697f
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-12-01 22:25:09 +0100

    Use React Helmet to add hard coded metadata
    title, lang and description to the head in Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,66 +1,74 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
+import { Helmet } from 'react-helmet';
 
 import Header from './Header'
 
-const Layout = ({ children }) => (
-  <>
-    <Global
-      styles={css`
-        * {
-          box-sizing: border-box;
-          margin: 0;
-        }
-
-        * + * {
-          margin-top: 1rem;
-        }
-
-        html,
-        body {
-          margin: 0;
-          color: #555;
-          font-size: 18px;
-          line-height: 1.4;
-
-          /* no margin on gatsby's div */
-          > div {
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Global
+        styles={css`
+          * {
+            box-sizing: border-box;
             margin: 0;
           }
-        }
 
-        h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6 {
-          color: #222;
-          line-height: 1.1;
-
-          + * {
-            margin-top: 0.5rem;
+          * + * {
+            margin-top: 1rem;
           }
-        }
 
-        strong {
-          color: #222;
-        }
+          html,
+          body {
+            margin: 0;
+            color: #555;
+            font-size: 18px;
+            line-height: 1.4;
 
-        li {
-          margin-top: 0.25rem;
-        }
-      `}
-    />
-    <Header />
-    <main css={css`
-      margin: 2rem auto 4rem;
-      max-width: 90vw;
-      width: 550px;
-    `}>
-      {children}
-    </main>
-  </>
-);
+            /* no margin on gatsby's div */
+            > div {
+              margin: 0;
+            }
+          }
+
+          h1,
+          h2,
+          h3,
+          h4,
+          h5,
+          h6 {
+            color: #222;
+            line-height: 1.1;
+
+            + * {
+              margin-top: 0.5rem;
+            }
+          }
+
+          strong {
+            color: #222;
+          }
+
+          li {
+            margin-top: 0.25rem;
+          }
+        `}
+      />
+      <Helmet>
+        <html lang="en" />
+        <title>Hello FEM!</title>
+        <meta name="description" content="site description" />
+      </Helmet>
+      <Header />
+      <main css={css`
+        margin: 2rem auto 4rem;
+        max-width: 90vw;
+        width: 550px;
+      `}>
+        {children}
+      </main>
+    </>
+  )
+};
 
 export default Layout;
