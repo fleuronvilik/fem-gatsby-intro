b9cc54c841282dacd6d6ed1f9e5ca0ea37793991
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-12-01 21:43:31 +0100

    Add a Layout Component that uses Emotion

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+import Layout from '../components/Layout'
+
 export default () => (
-  <>
-    <h1>Home</h1>
-    <p>Hello Gatsby!</p>
-    <Link to="/about/">Learn about me &larr;</Link>
-  </>
+  <Layout>
+    <h1>Welcome</h1>
+    <p>Learn to build blazing fast apps and websites with React using Gatsby.</p>
+    <Link to="/about/">Learn about me &rarr;</Link>
+  </Layout>
 );
