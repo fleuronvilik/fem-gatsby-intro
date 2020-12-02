1514ab56e5dd0a2fa977097f1e9a220deb384e36
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-12-02 22:19:41 +0100

    List posts as raw JSON on homepage

---
+++ src/hooks/use-posts.js
@@ -0,0 +1,25 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+export const usePosts = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMdx {
+        nodes {
+          frontmatter {
+            title
+            slug
+            author
+          }
+          excerpt
+        }
+      }
+    }
+  `);
+
+  return data.allMdx.nodes.map(post => ({
+    title: post.frontmatter.title,
+    slug: post.frontmatter.slug,
+    author: post.frontmatter.author,
+    excerpt: post.excerpt
+  }))
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,21 @@ import { Link } from 'gatsby';
 
 import Layout from '../components/Layout'
 
-export default () => (
-  <Layout>
-    <h1>Welcome</h1>
-    <p>Learn to build blazing fast apps and websites with React using Gatsby.</p>
-    <Link to="/about/">Learn about me &rarr;</Link>
-  </Layout>
-);
+import { usePosts } from '../hooks/use-posts'
+
+export default () => {
+  const posts = usePosts();
+
+  return (
+    <Layout>
+      <h1>Welcome</h1>
+      <p>Learn to build blazing fast apps and websites with React using Gatsby.</p>
+      <Link to="/about/">Learn about me &rarr;</Link>
+
+      <h2>Read my blog</h2>
+      {posts.map(post => (
+        <pre>{JSON.stringify(post, null, 2)}</pre>
+      ))}
+    </Layout>
+  );
+};
