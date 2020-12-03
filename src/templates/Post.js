8cd4e4c8260d01b519aecfed333167f910c5ae85
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-12-03 20:12:56 +0100

    Get posts data by slug

--- src/templates/Post.js
+++ src/templates/Post.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import { css } from '@emotion/core';
-/* import { MDXRenderer } from 'gatsby-plugin-mdx';
-import { graphql } from 'gatsby'; */
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/Layout';
 import ReadLink from '../components/ReadLink';
 
-//export const query = graphql
+export const query = graphql`
+  query($slug: String!) {
+    mdx(frontmatter: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        author
+      }
+      body
+    }
+  }
+`;
 
-const PostTemplate = ({ pageContext }) => (
+const PostTemplate = ({ data: { mdx:post }}) => (
   <Layout>
-    <h1>Post Title</h1>
-    <pre>{JSON.stringify(pageContext, null, 2)}</pre>
+    <h1>{post.frontmatter.title}</h1>
     <p css={css`font-size: .75rem;`}>
-      Posted by (author)
+      Posted by {post.frontmatter.author}
     </p>
+    <MDXRenderer>{post.body}</MDXRenderer>
     <p>Post Body goes here</p>
-    <ReadLink to='/'>back to all posts</ReadLink> 
+    <ReadLink to='/'>back to all posts &larr;</ReadLink> 
   </Layout>
 );
 
