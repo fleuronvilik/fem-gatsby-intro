ba6d6d2b14e264ac0c20c2f65afe0f7b5644f470
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-12-06 22:40:08 +0100

    Load IG images onto the homepage and add styling

--- src/components/Insta.js
+++ src/components/Insta.js
@@ -2,11 +2,52 @@ import React from 'react';
 import Image from 'gatsby-image';
 import { css } from '@emotion/core';
 
+import useInstagram from '../hooks/use-instagram'
+
 const Insta = () => {
+  const instaPhotos = useInstagram();
+
   return (
     <>
       <h2>Instagram posts from @_100daysofcode</h2>
-      <div>{/** show posts here **/}</div>
+      <div css={css`
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        margin: 1rem -0.5rem;
+      `
+      }>
+      {instaPhotos.map(photo => (
+        <a
+          key={photo.id}
+          href={`https://instagram.com/p/${photo.id}`}
+          css={css`
+            box-shadow: 0;
+            display: block;
+            margin: 0.5rem;
+            max-width: calc(33% - 1rem);
+            width: 120px;
+            transition: 200ms box-shadow linear;
+
+            :focus,
+            :hover {
+              box-shadow: 0 2px 14px #22222244;
+              z-index: 10;
+            }
+          `}
+        ><Image fluid={photo.fluid} css={css`
+            width: 100%;
+
+            * {
+              margin-top: 0;
+            }
+          `}
+          alt={photo.caption}
+          />
+        </a>
+        ))
+      }
+      </div>
       <a href={`https://www.instagram.com/_100DaysOfCode/`}>
         See more on Instagram &rarr;
       </a>
